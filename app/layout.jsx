ea9f7b6901202c663c5eff1c39342c728eeaa4dd
Shailesh <shailesh.shanbhags@gmail.com>
2026-07-21 23:44:42 +0530

    Added Navbar component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,11 @@
 import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
 
 const MainLayout = ({ children }) => {
   return (
     <html>
       <body>
+        <Navbar />
         <main>{children}</main>
       </body>
     </html>
