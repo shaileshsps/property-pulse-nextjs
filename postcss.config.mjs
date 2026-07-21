43e1c2f418b2efd0c79bf66ec3a8784a704f3701
Shailesh <shailesh.shanbhags@gmail.com>
2026-07-21 23:05:02 +0530

    Added assets and configured tailwind css

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,6 +2,16 @@ const config = {
   plugins: {
     "@tailwindcss/postcss": {},
   },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      gridTemplateColumns: {
+        '70/30': '70% 28%',
+      },
+    },
+  },
 };
 
 export default config;
