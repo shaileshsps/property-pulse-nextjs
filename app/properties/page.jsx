6dfe0ec323f75e2ea0652639118c97f11c7cfcb7
Shailesh <shailesh.shanbhags@gmail.com>
2026-07-24 23:29:32 +0530

    Added properties.json file and PropertyCard Component

---
+++ app/properties/page.jsx
@@ -0,0 +1,15 @@
+import properties from "@/properties.json";
+const Properties = () => {
+  return (
+    <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+          <PropertySearchForm />
+        </div>
+      </section>
+      <Properties />
+    </>
+  );
+};
+
+export default Properties;
